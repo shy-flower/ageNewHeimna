660c7aecb7d9aaabdc1cb68b481f78ccfad45837
danhongyan <759210630@qq.com>
2020-12-20 10:21:07 +0800

    登录界面开发01

---
+++ src/components/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bgColor">
+    <div class="verification">111</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.bgColor {
+  background-color: #194a6e;
+  height: 100%;
+  .verification {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    margin: auto;
+    bottom: 0;
+    height: 350px;
+    width: 450px;
+    background-color: white;
+    border-radius: 4px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import elementUI from 'element-ui'
+/* 引入element ui样式 */
+import 'element-ui/lib/theme-chalk/index.css'
+/* 引入全局样式 */
+import '@/assets/css/rest.css'
+
+
+
+
+Vue.use(elementUI)
+import{
+  
+}from 'element-ui'
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
