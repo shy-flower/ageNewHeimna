f9138ed9aa13993317dd53c2fd9385cf95493698
danhongyan <759210630@qq.com>
2020-12-22 23:03:01 +0800

    66

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,18 @@ import Home from '@/components/home.vue'
 import Welcome from '@/components/welcome.vue'
 Vue.use(VueRouter)
 
-const routes = [
-  {path:'/', redirect:"/home"},
- {path:'/home',component:Home},
- {path:"/welcome",component:Welcome}
+const routes = [{
+    path: '/',
+    redirect: "/home"
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: "/welcome",
+    component: Welcome
+  }
 ]
 
 const router = new VueRouter({
@@ -15,7 +23,7 @@ const router = new VueRouter({
 })
 
 /* 挂载路由导航守卫 */
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   /* 
   1. to代表将要访问的路径
   2. from代表从那个路径过来的
@@ -23,10 +31,10 @@ router.beforeEach((to,from,next)=>{
    next() 放行
    next('/home')代表强制跳转到指定路径页面 
   */
-  if(to.path ==='/home')  return next()
-  const tokenStr= window.sessionStorage.getItem('token')
-  if(!tokenStr){
-  return  next('/home')
+  if (to.path === '/home') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/home')
   }
   next()
 })
